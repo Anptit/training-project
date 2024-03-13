825da275ab245d5649c4ec7308ab32babf817869
Anptit <anph@bap.jp>
2024-03-13 18:04:27 +0700

    first commit

---
+++ resources/views/home/homepage.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
+    <title>Home</title>
+    <style>
+        html {
+            margin: 0 auto;
+            width: 1920px;
+            height: 8183px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        @include('layouts.heading')
+        
+    </div>
+</body>
+</html>
--- resources/views/layouts/heading.blade.php
+++ resources/views/layouts/heading.blade.php
@@ -1,1 +1,26 @@
-@extends('')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="heading">
+        <div class="heading_logo">
+            <img src="/assets/Logo.png" alt="logo">
+        </div>
+        <div class="heading_menu">
+            <ul class="heading_menu-list">
+                <li class="heading_menu-list-item">Home</li>
+                <li class="heading_menu-list-item">Page</li>
+                <li class="heading_menu-list-item">Services</li>
+                <li class="heading_menu-list-item">Project</li>
+                <li class="heading_menu-list-item">Blog</li>
+                <li class="heading_menu-list-item">Contact</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
