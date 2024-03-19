e005321e855f49cd344d1c950ff601edd24a8c8d
Anptit <anph@bap.jp>
2024-03-19 17:38:58 +0700

    create-footer-view

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Footer</title>
+</head>
+
+<body>
+    <div class="footer__container">
+        <ul class="footer__body--list">
+            <li class="footer__body--list-item">
+                <div class="footer__body--list-item-content">
+                    <img src="/assets/images/Logo.png" alt="" class="footer__body--list-item-content-logo">
+                    <p class="footer__body--list-item-content-description">
+                        It is a long established fact that a reader will be distracted lookings.
+                    </p>
+                </div>
+                <div class="footer__body--list-item-icons">
+                    <img src="/assets/footer/in.png" alt="" 
+                    class="footer__body--list-item-icons-item">
+                    <img src="/assets/footer/ins.png" alt="" 
+                    class="footer__body--list-item-icons-item">
+                    <img src="/assets/footer/tw.png" alt="" 
+                    class="footer__body--list-item-icons-item">
+                    <img src="/assets/footer/fb.png" alt="" 
+                    class="footer__body--list-item-icons-item">
+                </div>
+            </li>
+            <li class="footer__body--list-item">
+                <h2 class="footer__body--list-item-title">Pages</h2>
+                <ul class="footer__body--list-item-pages">
+                    <li class="footer__body--list-item-pages-item">
+                        <a href="#" class="footer__body--list-item-pages-item-link">
+                            About Us
+                        </a>
+                    </li>
+                    <li class="footer__body--list-item-pages-item">
+                        <a href="#" class="footer__body--list-item-pages-item-link">
+                            Our Project
+                        </a>
+                    </li>
+                    <li class="footer__body--list-item-pages-item">
+                        <a href="#" class="footer__body--list-item-pages-item-link">
+                            Our Team
+                        </a>
+                    </li>
+                    <li class="footer__body--list-item-pages-item">
+                        <a href="#" class="footer__body--list-item-pages-item-link">
+                            Contact Us
+                        </a>
+                    </li>
+                    <li class="footer__body--list-item-pages-item">
+                        <a href="#" class="footer__body--list-item-pages-item-link">
+                            Services
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="footer__body--list-item">
+                <h2 class="footer__body--list-item-title">Services</h2>
+                <ul class="footer__body--list-item-services">
+                    <li class="footer__body--list-item-services-item">
+                        <a href="#" class="footer__body--list-item-services-item-link">
+                            Kitchan
+                        </a>
+                    </li>
+                    <li class="footer__body--list-item-services-item">
+                        <a href="#" class="footer__body--list-item-services-item-link">
+                            Leaving Area
+                        </a>
+                    </li>
+                    <li class="footer__body--list-item-services-item">
+                        <a href="#" class="footer__body--list-item-services-item-link">
+                            Bathroom
+                        </a>
+                    </li>
+                    <li class="footer__body--list-item-services-item">
+                        <a href="#" class="footer__body--list-item-services-item-link">
+                            Dinning Hall
+                        </a>
+                    </li>
+                    <li class="footer__body--list-item-services-item">
+                        <a href="#" class="footer__body--list-item-services-item-link">
+                            Bedroom
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="footer__body--list-item">
+                <h2 class="footer__body--list-item-title">Services</h2>
+                <ul class="footer__body--list-item-contact">
+                    <li class="footer__body--list-item-contacts-item">
+                        <a href="#" class="footer__body--list-item-contacts-item-link">
+                            Kitchan
+                        </a>
+                    </li>
+                    <li class="footer__body--list-item-contacts-item">
+                        <a href="#" class="footer__body--list-item-contacts-item-link">
+                            Leaving Area
+                        </a>
+                    </li>
+                    <li class="footer__body--list-item-contacts-item">
+                        <a href="#" class="footer__body--list-item-contacts-item-link">
+                            Bathroom
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</body>
+
+</html>
